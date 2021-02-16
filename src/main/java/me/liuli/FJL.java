d14e8523d5ee694ceada574d19077b2917c26fc6
liulihaocai <liulihaocai@outlook.com>
2021-02-16 16:26:46 +0800

    stuff

---
+++ src/main/java/me/liuli/FJL.java
@@ -0,0 +1,10 @@
+package me.liuli;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class FJL extends PluginBase {
+    @Override
+    public void onEnable() {
+        this.getLogger().info("This is just a lib for Liuli's plugin.");
+    }
+}
